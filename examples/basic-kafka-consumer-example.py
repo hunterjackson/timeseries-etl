60544170dfb08c12fa05c566047add234d757d5f
Hunter Jackson <hjackson@sightmachine.com>
2018-01-17 13:24:15 -0800

    add deserialization to kafka consumer example

--- examples/basic-kafka-consumer-example.py
+++ examples/basic-kafka-consumer-example.py
@@ -1,5 +1,6 @@
 from kafka import KafkaConsumer
 from kafka import TopicPartition
+import json
 
 topic = 'test'
 hostname = 'kafka'
@@ -7,7 +8,8 @@ hostname = 'kafka'
 consumer = KafkaConsumer(# topic,  # only used in subscription mode
                          bootstrap_servers='kafka'  # hostname
                          # , group_id='python-test-group'  # consumer group, not going to use this method though
-                         , auto_offset_reset='earliest')  # tells the consumer to start from the queue if an offset is not present
+                         , auto_offset_reset='earliest'  # tells the consumer to start from the queue if an offset is not present
+                         , value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
 # assign to all partitions of topic, now assigned instead of subscribed
 consumer.assign([TopicPartition(topic, partition) for partition in consumer.partitions_for_topic(topic)])
