637c9f1223ca00586bb34341005135e70570130e
Hunter Jackson <hjackson@sightmachine.com>
2018-01-17 18:48:11 -0800

    add schema for transforms as well as schema validator

--- timeseries_etl/schema_validators.py
+++ timeseries_etl/schema_validators.py
@@ -1,7 +1,9 @@
 import jsonschema
 import json
 import pkg_resources
+import yaml
 
+# loading in all of the kafka schema here to avoid constantly reloading it
 resource_package = __name__
 kafka_msg_schema_stream = pkg_resources.resource_stream(resource_package, '/'.join(('schemas', 'kafka-message.schema.json')))
 kafka_msg_schema = json.load(kafka_msg_schema_stream)
@@ -10,10 +12,25 @@ kafka_msg_schema = json.load(kafka_msg_schema_stream)
 def validate_kafka_messsage(msg, schema=kafka_msg_schema):
     """
 
-    :param msg: json message to validate
+    :param msg: loaded json message to validate
     :param schema: schema to validate against
-    :return:
+    :return: boolean
     """
     jsonschema.validate(msg, schema, format_checker=jsonschema.FormatChecker())
+
+    return True
+
+
+def validate_transform_config(msg):
+    """
+
+    :param msg: loaded yaml message to validate
+    :return: boolean
+    """
+    rel_path_to_schema = '/'.join(('schemas', 'transform_configuration.schema.yaml'))
+    transform_config_stream = pkg_resources.resource_stream(resource_package, rel_path_to_schema)
+    transform_config_schema = yaml.load(transform_config_stream)
+
+    jsonschema.validate(msg, transform_config_schema)
 
     return True
